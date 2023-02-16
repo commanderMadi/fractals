197075866656910c83620c4d6dca69549dbf9d26
Ahmed Magdy <madimagdy@gmail.com>
2023-02-16 20:32:09 +0900

    rename square recursive func

--- sketch.js
+++ sketch.js
@@ -14,11 +14,11 @@ function setup() {
 
 function draw() {
     translate(width / 2, height / 2);
-    mySquare(startSize);
+    squareGenerator(startSize);
     noLoop();
 }
 
-function mySquare(side) {
+function squareGenerator(side) {
     side = side / 3;
     if (side >= 1) {
         fill(200 % side, 100 % side, 80 % side);
@@ -26,42 +26,42 @@ function mySquare(side) {
         // left center translation
         push();
         translate(-side, 0);
-        mySquare(side);
+        squareGenerator(side);
         pop();
         //left top translation
         push();
         translate(-side, -side);
-        mySquare(side);
+        squareGenerator(side);
         pop();
         // center top translation
         push();
         translate(0, -side);
-        mySquare(side);
+        squareGenerator(side);
         pop();
         // right top translation
         push();
         translate(side, -side);
-        mySquare(side);
+        squareGenerator(side);
         pop();
         // right center translation
         push();
         translate(side, 0);
-        mySquare(side);
+        squareGenerator(side);
         pop();
         // right bottom translation
         push();
         translate(side, side);
-        mySquare(side);
+        squareGenerator(side);
         pop();
         // center bottom translation
         push();
         translate(0, side);
-        mySquare(side);
+        squareGenerator(side);
         pop();
         // left bottom translation
         push();
         translate(-side, side);
-        mySquare(side);
+        squareGenerator(side);
         pop();
     }
 }
